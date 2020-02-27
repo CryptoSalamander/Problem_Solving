5069f274d1dec9fdbe12bf837372c9018ae854a2
Hyunsoo Luke HA <dhy03196@naver.com>
2020-02-27 16:51:37 +0900

    [ADD] 7568 Solved
    7568번 - 덩치

---
+++ Stepbystep/Level11/7568.cpp
@@ -0,0 +1,24 @@
+//
+// Created by HHS on 2020-02-27.
+//
+#include <iostream>
+#include <utility>
+#include <vector>
+using namespace std;
+
+int main() {
+    int num;
+    int rank = 1;
+    pair<int,int> arr[50];
+    cin >> num;
+    for(int i = 0; i < num; i++)
+        cin >> arr[i].first >> arr[i].second;
+    for(int i = 0; i < num; i++)
+    {
+        for(int j = 0; j < num; j++)
+            if(arr[i].first < arr[j].first && arr[i].second < arr[j].second)
+                rank++;
+        cout << rank << ' ';
+        rank = 1;
+    }
+}
