31c3a90e55f70933a5b25bb9f0e6e17a68b357da
Hyunsoo Luke HA <dhy03196@naver.com>
2020-03-17 06:16:59 +0900

    [ADD] 2523 Solved
    실습1 - 별 찍기 12

---
+++ Stepbystep/Practice1/2523.cpp
@@ -0,0 +1,22 @@
+//
+// Created by HHS on 2020-03-17.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for(int j = 0; j < i; j++)
+            cout << '*';
+        cout << '\n';
+    }
+    for(int i = n-1; i > 0; i--)
+    {
+        for(int j = 0; j < i; j++)
+            cout << '*';
+        cout << '\n';
+    }
+}
