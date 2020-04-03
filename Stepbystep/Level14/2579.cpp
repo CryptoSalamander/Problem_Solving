50e70b8b9e87d06de8a868a5a291f56a13fd752e
CryptoSalamander <dhy03196@naver.com>
2020-04-03 21:57:52 +0900

    [ADD] 2579 Solved

---
+++ Stepbystep/Level14/2579.cpp
@@ -0,0 +1,19 @@
+//
+// Created by 하현수 on 2020/04/02.
+//
+#include <iostream>
+using namespace std;
+int stairs[301];
+int dp[301];
+int N;
+int main() {
+    cin >> N;
+    for(int i = 1; i <= N; i++)
+        cin >> stairs[i];
+    dp[1] = stairs[1];
+    dp[2] = stairs[1]+stairs[2];
+    dp[3] = max(stairs[1],stairs[2])+stairs[3];
+    for(int i = 4; i <= N; i++)
+        dp[i] = max(dp[i-2] + stairs[i],dp[i-3]+stairs[i-1]+stairs[i]);
+    cout << dp[N];
+}
