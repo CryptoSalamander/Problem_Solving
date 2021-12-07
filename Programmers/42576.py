8ab3b277238f7db33fb81a7d6792f65bbe690e8a
HYUNSOO HA <dhy03196@naver.com>
2021-12-07 11:37:15 +0900

    [ADD] 42576 with python

---
+++ Programmers/42576.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+def solution(participant, completion):
+    dic = defaultdict(int)
+    for p in participant:
+        dic[p] += 1
+    for c in completion:
+        dic[c] -= 1
+        if dic[c] == 0:
+            del dic[c]
+    return list(dic.keys())[0]
